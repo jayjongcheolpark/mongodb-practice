ca1cc1144715ff26e415c1e43af553ed60178311
gogolearn <jay.jongcheol.park@gmail.com>
2017-10-12 16:38:34 -0400

    Default promise implementation

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,14 +1,21 @@
 const mongoose = require('mongoose');
-const uri = 'mongodb://localhost/users_test';
-mongoose.connect(uri, {
-    userMongoClient: true,
+
+mongoose.Promise = global.Promise;
+
+before(done => {
+  const uri = 'mongodb://localhost/users_test';
+  mongoose.connect(uri, {
+      userMongoClient: true,
+  });
+
+  mongoose.connection
+    .once('open', () => done())
+    .on('error', (error) => {
+      console.warn('Warning', error);
+    });
 });
 
-mongoose.connection
-  .once('open', () => console.log('Good to go!'))
-  .on('error', (error) => {
-    console.warn('Warning', error);
-  });
+
 
 beforeEach(done => {
   mongoose.connection.collections.users.drop(() => {
