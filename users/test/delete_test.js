b5590d350eb7802e81c18e246b583a31c42ffb12
gogolearn <jay.jongcheol.park@gmail.com>
2017-10-13 21:54:31 -0400

    Class based removals

--- users/test/delete_test.js
+++ users/test/delete_test.js
@@ -17,8 +17,14 @@ describe('Deleting a user', () => {
       });
   });
 
-  it('class method remove', () => {
-
+  it('class method remove', done => {
+    // Remove a bunch of records with some given criteria
+    User.remove({ name: 'Joe' })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then(user => {
+        assert(user === null);
+        done();
+      });
   });
 
   it('class method findAndRemove', () => {
