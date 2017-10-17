96c672144a22db30dfcf6eae4a8240fcc314a271
gogolearn <jay.jongcheol.park@gmail.com>
2017-10-17 00:17:08 -0400

    Wiring up has many and has one

--- users/src/user.js
+++ users/src/user.js
@@ -12,7 +12,11 @@ const UserSchema = new Schema({
     required: [true, 'Name is required.']
   },
   posts: [PostSchema],
-  likes: Number
+  likes: Number,
+  blogPosts: [{
+    type: Schema.Types.ObjectId,
+    ref: 'blogPost'
+  }]
 });
 
 // we cannot use fat arrow function because of this scope
--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -8,7 +8,7 @@ before(done => {
   var promise = mongoose.connect(uri, {
       userMongoClient: true
   });
-  
+
   mongoose.connection
     .once('open', () => done())
     .on('error', (error) => {
@@ -19,8 +19,17 @@ before(done => {
 
 
 beforeEach(done => {
-  mongoose.connection.collections.users.drop(() => {
-    // Ready to run the next test
-    done();
+
+  // model(name, schema, collectionName)
+  // if you didn't define collectionName,
+  // mongoose automatically adds 's' to model's name for collectionName
+  // collectionName should be lowercase
+  const { users, comments, blogposts } = mongoose.connection.collections;
+  users.drop(() => {
+    comments.drop(() => {
+      blogposts.drop(() => {
+        done();
+      });
+    });
   });
 });
