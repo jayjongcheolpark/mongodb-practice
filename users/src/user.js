6fb13a720aa572e75b7ca83cd7d3bbfff9d60139
gogolearn <jay.jongcheol.park@gmail.com>
2017-10-16 23:19:44 -0400

    Fixing update tests

--- users/src/user.js
+++ users/src/user.js
@@ -11,7 +11,8 @@ const UserSchema = new Schema({
     },
     required: [true, 'Name is required.']
   },
-  posts: [PostSchema]
+  posts: [PostSchema],
+  likes: Number
 });
 
 // we cannot use fat arrow function because of this scope
