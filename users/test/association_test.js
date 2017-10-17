83ee2a8e35ea433e8dadeab378bc5877d23eac4c
gogolearn <jay.jongcheol.park@gmail.com>
2017-10-17 00:26:38 -0400

    Promise.all for parallel operations

--- users/test/association_test.js
+++ users/test/association_test.js
@@ -18,5 +18,15 @@ describe('Associations', () => {
     joe.blogPosts.push(blogPost);
     blogPost.comments.push(comment);
     comment.user = joe;
+
+    Promise.all([joe.save(), blogPost.save(), comment.save()])
+      .then(() => done());
+  });
+
+  it.only('saves a relation between a user and a blogpost', done => {
+    User.findOne({ name: 'Joe' })
+      .then(user => {
+        done();
+      });
   });
 });
